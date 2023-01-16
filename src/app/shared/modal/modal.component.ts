c5588893aea20e662bf87a9959f92ed7dd037e66
Artur <helter@wp.pl>
2023-01-16 16:33:22 +0100

    add portal functionality to modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,12 +6,16 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input() modalID = '';
 
-  constructor(public modal: ModalService) {}
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   closeModal() {
     this.modal.toggleModal(this.modalID);
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 }
